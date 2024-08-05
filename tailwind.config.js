116e66c5f5679590d98d603ce30101bb8939a7dc
Kevin Huang <qw820301@gmail.com>
2024-08-06 01:46:05 +0800

    建立專案並完成header跟footer的版型以及RWD。

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,25 @@
-/** @type {import('tailwindcss').Config} */
+/* @type {import('tailwindcss').Config} */
 export default {
   content: ['./pages/**/*.html', './layout/**/*.ejs', './main.js'],
   theme: {
     extend: {
       colors: {
-        primary: '#69F0AE',
+        primary: "#aa0601",
+        secondary: "#fbf2f2",
+        pink: "#FBF2F2",
       },
     },
+    container: {
+      center: true,
+      padding: '12px', // 預設的內邊距
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '992px',
+        xl: '1320px', // 將 xl 的最大寬度設置為 1320px
+        '2xl': '1536px',
+      },
+    }
   },
   plugins: [require('@tailwindcss/forms')],
 };
